782c0d334c45701211ec43dafa836e3dba69c790
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-18 15:32:06 -0300

    feat: criando classe que gera UUID

---
+++ src/@thirdparty/domain/value-objects/unique-id/unique-id.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4, validate as uuidValidate} from "uuid";
+import { ValueObject } from "../value-object";
+
+export class UniqueEntityUUID extends ValueObject<string> {
+  constructor(readonly id?: string) {
+    super(id ?? UniqueEntityUUID.generate());
+    this.validate(id ?? this.value);
+  }
+
+  static generate(): string {
+    const id = uuidv4();
+    return id;
+  }
+
+  private validate(id: string){
+    const isValid = uuidValidate(id);
+    if(!isValid){
+      throw new Error("id invalido");
+    }
+  }
+}
