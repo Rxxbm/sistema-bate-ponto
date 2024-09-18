4899a30433706c7130b3e7235d029902ea38a887
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-18 16:13:35 -0300

    feat: criando uma classe abstrata de um repositorio para generalizar uma entidade

---
+++ src/@thirdparty/domain/repositories/in-memory.repository.ts
@@ -0,0 +1,44 @@
+import { UniqueEntityUUID } from "../value-objects/unique-id/unique-id";
+import { RepositoryInterface } from "./in-memory.repository.interface";
+
+interface EntityWithId {
+  id: string | UniqueEntityUUID;
+}
+
+export abstract class InMemoryRepository<E extends EntityWithId>
+  implements RepositoryInterface<E>
+{
+  protected entities: E[] = [];
+
+  async save(entity: E): Promise<void> {
+    this.entities.push(entity);
+  }
+
+  async delete(id: string | UniqueEntityUUID): Promise<void> {
+    await this._getid(id);
+    this.entities = this.entities.filter((entity) => entity.id !== id);
+  }
+
+  async update(entity: E): Promise<any> {
+    await this._getid(entity.id);
+    const index = this.entities.findIndex((e) => e.id === entity.id);
+    this.entities[index] = entity;
+  }
+
+  async findById(id: string | UniqueEntityUUID): Promise<E> {
+    const item = await this._getid(id);
+    return item;
+  }
+
+  async findAll(): Promise<any[]> {
+    return this.entities;
+  }
+
+  async _getid(id: string | UniqueEntityUUID): Promise<E> {
+    const item = this.entities.find((entity) => entity.id === id);
+    if (!item) {
+      throw new Error(`Entidade não encontrada com o id ${id}`);
+    }
+    return item;
+  }
+}
