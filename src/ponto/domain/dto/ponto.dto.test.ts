b41df52830b96c40ef9aa30a0c5cec3a5e8edfd7
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-24 23:32:23 -0300

    test: testando o campo de empresa_id no dto de ponto

--- src/ponto/domain/dto/ponto.dto.test.ts
+++ src/ponto/domain/dto/ponto.dto.test.ts
@@ -59,4 +59,33 @@ describe("PontoDTO", () => {
       expect(errors).toContain("O ID do funcionário deve ser um UUID válido.");
     });
   });
+  describe("empresa_id", () => {
+    it("não deve validar um empresa_id vazio", async () => {
+      const pontoDTO = new PontoDTO({
+        id: "550e8400-e29b-41d4-a716-446655440000",
+        funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
+        empresa_id: "",
+        checkin: new Date(),
+        checkout: new Date(),
+      });
+
+      const errors = await pontoDTO.validate();
+      expect(errors.length).not.toBeNull();
+      expect(errors).toContain("O ID da empresa não pode estar vazio.");
+    });
+
+    it("não deve validar um empresa_id invalido", async () => {
+      const pontoDTO = new PontoDTO({
+        id: "550e8400-e29b-41d4-a716-446655440000",
+        funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
+        empresa_id: "invalid-id",
+        checkin: new Date(),
+        checkout: new Date(),
+      });
+
+      const errors = await pontoDTO.validate();
+      expect(errors.length).not.toBeNull();
+      expect(errors).toContain("O ID da empresa deve ser um UUID válido.");
+    });
+  });
 });
