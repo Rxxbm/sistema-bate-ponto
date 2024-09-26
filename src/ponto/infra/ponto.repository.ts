7236ca5b24d474e9aa2b67b605a7f80f6154bb9d
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-26 15:43:27 -0300

    feat: adicionando função para buscar pontos abertos na interface do repositório do ponto

--- src/ponto/infra/ponto.repository.ts
+++ src/ponto/infra/ponto.repository.ts
@@ -1,4 +1,18 @@
 import { InMemoryRepository } from "../../@thirdparty/domain/repositories/in-memory.repository";
 import { Ponto } from "../domain/ponto";
 
-export class PontoInMemoryRepository extends InMemoryRepository<Ponto> {}
+export interface PontoRepository extends InMemoryRepository<Ponto> {
+  findOpenPontoByFuncionarioId(
+    funcionario_id: string
+  ): Promise<Ponto | undefined>;
+}
+
+export class PontoInMemoryRepository extends InMemoryRepository<Ponto> {
+  async findOpenPontoByFuncionarioId(
+    funcionario_id: string
+  ): Promise<Ponto | undefined> {
+    return this.entities.find(
+      (ponto) => ponto.funcionario_id === funcionario_id && !ponto.checkout
+    );
+  }
+}
