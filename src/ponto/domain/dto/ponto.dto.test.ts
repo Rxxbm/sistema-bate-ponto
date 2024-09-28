9ce3042e80f4181fdd4df2fc7e5e16a48c2eff6f
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-28 18:17:08 -0300

    refactor: refatorando os testes do ponto.dto

--- src/ponto/domain/dto/ponto.dto.test.ts
+++ src/ponto/domain/dto/ponto.dto.test.ts
@@ -1,43 +1,11 @@
 import { PontoDTO } from "./ponto.dto";
 
 describe("PontoDTO", () => {
-  describe("ID", () => {
-    it("não deve validar um id vazio", async () => {
-      const pontoDTO = new PontoDTO({
-        id: "",
-        funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
-        empresa_id: "550e8400-e29b-41d4-a716-446655440002",
-        checkin: new Date(),
-        checkout: new Date(),
-      });
-
-      const errors = await pontoDTO.validate();
-      expect(errors.length).not.toBeNull();
-      expect(errors).toContain("O ID não pode estar vazio.");
-    });
-
-    it("não deve validar um id invalido", async () => {
-      const pontoDTO = new PontoDTO({
-        id: "invalid-id",
-        funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
-        empresa_id: "550e8400-e29b-41d4-a716-446655440002",
-        checkin: new Date(),
-        checkout: new Date(),
-      });
-
-      const errors = await pontoDTO.validate();
-      expect(errors.length).not.toBeNull();
-      expect(errors).toContain("O ID deve ser um UUID válido.");
-    });
-  });
   describe("funcionario_id", () => {
     it("não deve validar um funcionario_id vazio", async () => {
       const pontoDTO = new PontoDTO({
-        id: "550e8400-e29b-41d4-a716-446655440000",
         funcionario_id: "",
         empresa_id: "550e8400-e29b-41d4-a716-446655440002",
-        checkin: new Date(),
-        checkout: new Date(),
       });
 
       const errors = await pontoDTO.validate();
@@ -47,11 +15,8 @@ describe("PontoDTO", () => {
 
     it("não deve validar um funcionario_id invalido", async () => {
       const pontoDTO = new PontoDTO({
-        id: "550e8400-e29b-41d4-a716-446655440000",
         funcionario_id: "invalid-id",
         empresa_id: "550e8400-e29b-41d4-a716-446655440002",
-        checkin: new Date(),
-        checkout: new Date(),
       });
 
       const errors = await pontoDTO.validate();
@@ -62,11 +27,8 @@ describe("PontoDTO", () => {
   describe("empresa_id", () => {
     it("não deve validar um empresa_id vazio", async () => {
       const pontoDTO = new PontoDTO({
-        id: "550e8400-e29b-41d4-a716-446655440000",
         funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
         empresa_id: "",
-        checkin: new Date(),
-        checkout: new Date(),
       });
 
       const errors = await pontoDTO.validate();
@@ -76,16 +38,46 @@ describe("PontoDTO", () => {
 
     it("não deve validar um empresa_id invalido", async () => {
       const pontoDTO = new PontoDTO({
-        id: "550e8400-e29b-41d4-a716-446655440000",
         funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
         empresa_id: "invalid-id",
-        checkin: new Date(),
-        checkout: new Date(),
       });
 
       const errors = await pontoDTO.validate();
       expect(errors.length).not.toBeNull();
       expect(errors).toContain("O ID da empresa deve ser um UUID válido.");
     });
+  });
+
+  it("deve validar um pontoDTO válido", async () => {
+    const pontoDTO = new PontoDTO({
+      funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
+      empresa_id: "550e8400-e29b-41d4-a716-446655440002",
+    });
+
+    const errors = await pontoDTO.validate();
+    expect(errors.length).toBe(0);
+  });
+
+  it("garantir que formatErrors seja chamado", async () => {
+    const pontoDTO = new PontoDTO({
+      funcionario_id: "",
+      empresa_id: "invalid-id",
+    });
+
+    const spy = jest.spyOn(pontoDTO as any, "formatErrors");
+
+    await pontoDTO.validate();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it("deve validar um pontoDTO válido", async () => {
+    const pontoDTO = new PontoDTO({
+      funcionario_id: "550e8400-e29b-41d4-a716-446655440001",
+      empresa_id: "550e8400-e29b-41d4-a716-446655440002",
+    });
+
+    const errors = await pontoDTO.validate();
+    expect(errors.length).toBe(0);
   });
 });
