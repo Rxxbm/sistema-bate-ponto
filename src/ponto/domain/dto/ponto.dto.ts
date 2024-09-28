96ffe9c05e50c34c9bb3f0517f99c2d015069762
Rubem Neto <rubemcorrea0@gmail.com>
2024-09-28 18:15:19 -0300

    fix: corrigindo atributos desnecessários no pontoDTO

--- src/ponto/domain/dto/ponto.dto.ts
+++ src/ponto/domain/dto/ponto.dto.ts
@@ -1,17 +1,6 @@
-import {
-  IsUUID,
-  IsNotEmpty,
-  IsOptional,
-  IsDate,
-  validate,
-  ValidationError,
-} from "class-validator";
+import { IsUUID, IsNotEmpty, validate, ValidationError } from "class-validator";
 
 export class PontoDTO {
-  @IsUUID("4", { message: "O ID deve ser um UUID válido." })
-  @IsNotEmpty({ message: "O ID não pode estar vazio." })
-  id: string;
-
   @IsUUID("4", { message: "O ID do funcionário deve ser um UUID válido." })
   @IsNotEmpty({ message: "O ID do funcionário não pode estar vazio." })
   funcionario_id: string;
@@ -19,14 +8,6 @@ export class PontoDTO {
   @IsUUID("4", { message: "O ID da empresa deve ser um UUID válido." })
   @IsNotEmpty({ message: "O ID da empresa não pode estar vazio." })
   empresa_id: string;
-
-  @IsOptional()
-  @IsDate({ message: "A data de checkin deve ser uma data válida." })
-  checkin?: Date;
-
-  @IsOptional()
-  @IsDate({ message: "A data de checkout deve ser uma data válida." })
-  checkout?: Date;
 
   constructor(partial: Partial<PontoDTO>) {
     Object.assign(this, partial);
@@ -46,7 +27,6 @@ export class PontoDTO {
         if (error.constraints) {
           return Object.values(error.constraints);
         }
-        return [];
       })
       .flat();
   }
