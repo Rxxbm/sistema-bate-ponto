8126f95a8cd26bf22d6d2cc4e31db10891175e0f
Rubem Neto <rubemcorrea0@gmail.com>
2024-10-02 16:31:24 -0300

    feat: acrescentando funções findAllOpenPontoByEmpresaId e findAllClosedPontoByEmpresaID no PontoInMemoryRepository

--- src/ponto/infra/ponto.repository.ts
+++ src/ponto/infra/ponto.repository.ts
@@ -5,6 +5,9 @@ export interface PontoRepository extends InMemoryRepository<Ponto> {
   findOpenPontoByFuncionarioId(
     funcionario_id: string
   ): Promise<Ponto | undefined>;
+  findByEmpresaId(empresa_id: string): Promise<Ponto[]>;
+  findAllOpenPontoByEmpresaId(empresa_id: string): Promise<Ponto[]>;
+  findAllClosedPontoByEmpresaId(empresa_id: string): Promise<Ponto[]>;
 }
 
 export class PontoInMemoryRepository extends InMemoryRepository<Ponto> {
@@ -13,6 +16,22 @@ export class PontoInMemoryRepository extends InMemoryRepository<Ponto> {
   ): Promise<Ponto | undefined> {
     return this.entities.find(
       (ponto) => ponto.funcionario_id === funcionario_id && !ponto.checkout
+    );
+  }
+
+  async findByEmpresaId(empresa_id: string): Promise<Ponto[]> {
+    return this.entities.filter((ponto) => ponto.empresa_id === empresa_id);
+  }
+
+  async findAllOpenPontoByEmpresaId(empresa_id: string): Promise<Ponto[]> {
+    return this.entities.filter(
+      (ponto) => !ponto.checkout && ponto.empresa_id === empresa_id
+    );
+  }
+
+  async findAllClosedPontoByEmpresaId(empresa_id: string): Promise<Ponto[]> {
+    return this.entities.filter(
+      (ponto) => ponto.checkout && ponto.empresa_id === empresa_id
     );
   }
 }
