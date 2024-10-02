56da9541e4ff79d6c84a026fa0f0f1f77855c566
Rubem Neto <rubemcorrea0@gmail.com>
2024-10-02 19:07:54 -0300

    feat: criando caso de uso para listar todos os pontos de um funcionario por funcionario_id

---
+++ src/ponto/use-case/list-all-funcionarios-ponto.usecase.ts
@@ -0,0 +1,38 @@
+import { UseCase } from "../../@thirdparty/use-case/use-case.interface";
+import { Ponto } from "../domain/ponto";
+import { PontoRepository } from "../infra/ponto.repository";
+
+export class ListEmpresaPontosUseCase implements UseCase<Input, Output> {
+  constructor(private readonly pontoRepository: PontoRepository) {}
+
+  async execute(data: Input): Promise<Output> {
+    const { funcionario_id, status } = data;
+
+    if (status === "aberto") {
+      const ponto = await this.pontoRepository.findOpenPontoByFuncionarioId(
+        funcionario_id
+      );
+      return ponto;
+    }
+
+    if (status === "fechado") {
+      const pontos = await this.pontoRepository.findAllClosedPontoByEmpresaId(
+        funcionario_id
+      );
+      return pontos;
+    }
+
+    const pontos = await this.pontoRepository.findAllPontoByFuncionarioId(
+      funcionario_id
+    );
+
+    return pontos;
+  }
+}
+
+export type Input = {
+  funcionario_id: string;
+  status?: string;
+};
+
+export type Output = Ponto[] | Ponto;
