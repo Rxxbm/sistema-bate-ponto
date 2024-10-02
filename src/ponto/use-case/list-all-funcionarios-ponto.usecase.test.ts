1b635c4d116acd5b5e8bfa46cef9025d21f7b4e5
Rubem Neto <rubemcorrea0@gmail.com>
2024-10-02 19:08:29 -0300

    test: garantindo que deve retorna pontos abertos de funcionario_id

---
+++ src/ponto/use-case/list-all-funcionarios-ponto.usecase.test.ts
@@ -0,0 +1,62 @@
+import {
+  ListEmpresaPontosUseCase,
+  Input,
+} from "./list-all-funcionarios-ponto.usecase";
+import {
+  PontoInMemoryRepository,
+  PontoRepository,
+} from "../infra/ponto.repository";
+import { Ponto } from "../domain/ponto";
+
+describe("ListEmpresaPontosUseCase", () => {
+  let pontoRepository: PontoRepository;
+  let useCase: ListEmpresaPontosUseCase;
+
+  const arrange = [
+    new Ponto({ funcionario_id: "john", empresa_id: "1" }),
+    new Ponto({ funcionario_id: "jane", empresa_id: "2" }),
+    new Ponto({ funcionario_id: "john", empresa_id: "1" }),
+    new Ponto({ funcionario_id: "jane", empresa_id: "2" }),
+    new Ponto({ funcionario_id: "john", empresa_id: "1" }),
+    new Ponto({ funcionario_id: "jane", empresa_id: "2" }),
+    new Ponto({ funcionario_id: "john", empresa_id: "1" }),
+    new Ponto({ funcionario_id: "jane", empresa_id: "2" }),
+  ] as Ponto[];
+
+  for (let i = 0; i < arrange.length - 2; i++) {
+    arrange[i].fechar_ponto();
+  }
+
+  const closedPontos = arrange.filter((ponto) => ponto.checkout);
+
+  const openPontos = arrange.filter((ponto) => !ponto.checkout);
+
+  beforeEach(() => {
+    pontoRepository = new PontoInMemoryRepository();
+
+    arrange.forEach(async (ponto) => {
+      await pontoRepository.save(ponto);
+    });
+    useCase = new ListEmpresaPontosUseCase(pontoRepository);
+  });
+
+  it("deve retorna pontos abertos de funcionario_id", async () => {
+    let input: Input = { funcionario_id: "jane", status: "aberto" };
+    let expectedPonto: Ponto = openPontos.filter(
+      (ponto) => ponto.funcionario_id === "jane"
+    )[0];
+
+    let result = await useCase.execute(input);
+
+    expect(result).toEqual(expectedPonto);
+
+    input = { funcionario_id: "john", status: "aberto" };
+    expectedPonto = openPontos.filter(
+      (ponto) => ponto.funcionario_id === "john"
+    )[0];
+
+    result = await useCase.execute(input);
+
+    expect(result).toEqual(expectedPonto);
+  });
+});
