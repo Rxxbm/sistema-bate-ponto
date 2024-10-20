aa73b37c8063f75eaddb8bb39297380f8d3286d4
Rubem Neto <rubemcorrea0@gmail.com>
2024-10-20 17:02:11 -0300

    chore: importação do modulo de usuario no app principal do nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PontoModule } from './ponto/ponto.module';
 import { EmpresaModule } from "./empresa/empresa.module";
 import { ConfiguracaoModule } from "./configuracoes/configuracoes.module";
 import { FuncionarioModule } from "./funcionario/funcionario.module";
+import { UsuarioModule } from "./usuario/usuario.module";
 
 @Module({
-  imports: [PontoModule, EmpresaModule, ConfiguracaoModule, FuncionarioModule],
+  imports: [PontoModule, EmpresaModule, ConfiguracaoModule, FuncionarioModule, UsuarioModule],
   controllers: [],
   providers: [],
 })
