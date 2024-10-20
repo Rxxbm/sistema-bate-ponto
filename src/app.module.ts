c856092dda2e11ef15d5ca4b307b8c969936ea1e
Rubem Neto <rubemcorrea0@gmail.com>
2024-10-20 14:16:11 -0300

    chore: importando modulo de funcionario app principal do nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { PontoModule } from './ponto/ponto.module';
 import { EmpresaModule } from "./empresa/empresa.module";
 import { ConfiguracaoModule } from "./configuracoes/configuracoes.module";
+import { FuncionarioModule } from "./funcionario/funcionario.module";
 
 @Module({
-  imports: [PontoModule, EmpresaModule, ConfiguracaoModule],
+  imports: [PontoModule, EmpresaModule, ConfiguracaoModule, FuncionarioModule],
   controllers: [],
   providers: [],
 })
